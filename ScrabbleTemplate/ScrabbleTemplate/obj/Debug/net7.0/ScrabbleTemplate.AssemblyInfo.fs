35bb2d54a378bca433b7734e8d0b1ad3ad34b0bd
Kejzer <Kaare.Jensen@live.dk>
2024-05-13 21:40:19 +0200

    First attempt at a trie dict

--- ScrabbleTemplate/ScrabbleTemplate/obj/Debug/net7.0/ScrabbleTemplate.AssemblyInfo.fs
+++ ScrabbleTemplate/ScrabbleTemplate/obj/Debug/net7.0/ScrabbleTemplate.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("ScrabbleTemplate")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39ce48349e18341a6e5cbb5cabc0f2478d9c2fc2")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f74d7a81bba85294aa556ad65e5ade9d6f64ffb")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("ScrabbleTemplate")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("ScrabbleTemplate")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
